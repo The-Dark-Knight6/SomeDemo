291f0cb60ebc3544a1fb5564e92344c983d63cc1
justone <645865839@qq.com>
2019-10-28 14:44:26 +0800

    120

---
+++ again/js/navs.js
@@ -0,0 +1,8 @@
+let navs = ` <ul>
+<li><a href="./jqtest2.html">首页</a></li>
+<li><a href="./jqtest3.html">第二页_测试</a></li>
+<li><a href="./guessnum.html">猜数字</a></li>
+<li><a href="./pressgame.html">敲击键盘</a></li>
+<li><a href="javascript:;">five page</a></li>
+<li><a href="javascript:;">six page</a></li>
+</ul>`;
