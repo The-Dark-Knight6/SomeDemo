a62cd57c1e0ad8a44947513bc4404c57ac2ad4f4
justone <645865839@qq.com>
2019-10-29 13:44:49 +0800

    sd23

--- again/js/navs.js
+++ again/js/navs.js
@@ -1,8 +1,8 @@
-let navs = ` <ul>
-<li><a href="./jqtest2.html">首页</a></li>
-<li><a href="./jqtest3.html">第二页_测试</a></li>
+let navs = ` <span>正</span><ul>
+<li><a href="./first.html">首页</a></li>
+<li><a href="./jqtest3.html">验证码</a></li>
 <li><a href="./guessnum.html">猜数字</a></li>
 <li><a href="./pressgame.html">敲击键盘</a></li>
-<li><a href="./js_table.html">js_table</a></li>
-<li><a href="./calculate.html">js_calculate</a></li>
+<li><a href="./js_table.html">表格</a></li>
+<li><a href="./calculate.html">计算器</a></li>
 </ul>`;
