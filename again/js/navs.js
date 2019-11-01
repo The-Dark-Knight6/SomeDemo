ec31ea98231a3723e30b3e111530cecffa20a370
justone <645865839@qq.com>
2019-11-01 17:44:23 +0800

    asd2

--- again/js/navs.js
+++ again/js/navs.js
@@ -2,8 +2,9 @@ let navs = ` <span>正</span><ul>
 <li><a href="./first.html">首页</a></li>
 <li><a href="./jqtest3.html">验证码</a></li>
 <li><a href="./guessnum.html">猜数字</a></li>
-<li><a href="./pressgame.html">敲击键盘</a></li>
-<li><a href="./js_table.html">表格</a></li>
+<li><a href="./pressgame.html">敲字母</a></li>
+<li><a href="./js_table.html">表格检索</a></li>
 <li><a href="./calculate.html">计算器</a></li>
 <li><a href="./divtable.html">自制列表</a></li>
+<li><a href="./Echarts.html">图表</a></li>
 </ul>`;
