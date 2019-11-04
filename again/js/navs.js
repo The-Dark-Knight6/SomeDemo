42d14a14aeb3be4a43bc22c918c0842fd4da5076
justone <645865839@qq.com>
2019-11-04 16:50:05 +0800

    asd23

--- again/js/navs.js
+++ again/js/navs.js
@@ -8,3 +8,35 @@ let navs = ` <span>正</span><ul>
 <li><a href="./divtable.html">自制列表</a></li>
 <li><a href="./Echarts.html">图表</a></li>
 </ul>`;
+
+let float=` <div class="float_tip">
+<div class="fly_cont">
+    <p></p>
+</div>
+</div>`
+
+// window.onload=function(){
+//     $('a').mouseover(function(){
+//         $('.float_tip').show();
+//         $('.fly_cont>p').text($(this).text());
+//     })
+//     $('a').mouseleave(function(){
+//         $('.float_tip').hide();
+//     })
+// }
+
+window.onload=function(){
+    let a_name=document.getElementsByTagName('a');
+    let float_tip = document.querySelector('.float_tip');
+    let fly_cont = document.querySelector('.fly_cont').children[0];
+    for(let i = 0;i < a_name.length ; i++){
+        a_name[i].addEventListener('mouseover',function(){
+            float_tip.style.display='block';
+            fly_cont.innerHTML = this.innerHTML;
+        })
+        a_name[i].addEventListener('mouseleave',function(){
+            float_tip.style.display='none';
+        })
+    }
+}
+    
