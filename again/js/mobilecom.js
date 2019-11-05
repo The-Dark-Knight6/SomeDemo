a32ec5e401f9f87968f951f09985d50c2e7f5b0f
justone <645865839@qq.com>
2019-11-05 18:09:32 +0800

    sd23

--- again/js/mobilecom.js
+++ again/js/mobilecom.js
@@ -1,8 +1,28 @@
 let phone_top=` <div class="nav_content">
 <img src="./img/menu.png" alt="" class="choice">
 <img src="./img/ico2.ico" alt="">
-<img src="./img/mus.svg" alt="">
+<img src="./img/mus.svg" alt="" class="music">
 </div>`;
+
+let nav_list=` <ul>
+<li>
+      <h3>index</h3>
+      <ul>
+            <li>
+                  <a href="./PhoneIndex.html">index-page</a>
+            </li>
+            <li>
+                  <a href="./havafun.html">transition</a>
+            </li>
+      </ul>
+</li>
+<li>
+      <h3>coding</h3>
+</li>
+<li>
+      <h3>writing</h3>
+</li>
+</ul>`;
 
 if (os.isAndroid || os.isPhone) {
     console.log('phone')
@@ -10,3 +30,23 @@ if (os.isAndroid || os.isPhone) {
     window.location.replace('./index.html');
     console.log('computer');
 }
+
+window.onload = function(){
+   let audio = document.createElement('audio');
+   audio.src='./img/qing.mp3';
+   $('.music').click(function(){
+       if(audio.paused){
+        audio.play();
+       }else{
+        audio.pause();
+       }
+   })
+    $('.choice').click(function (e) {
+    e.stopPropagation(); //阻止事件冒泡
+    $('.left_nav').toggle(500);
+        })
+    $(document).click(function () {
+    $('.left_nav').hide(500);   
+        })
+}
+
