2b2bd672c3572c467297c6d8dfb0dade9cd2910f
justone <645865839@qq.com>
2019-11-07 18:09:01 +0800

    asd23

--- again/js/footer.js
+++ again/js/footer.js
@@ -5,3 +5,55 @@ let foot = ` <div class="foot">
 <p>Copyright @ 2019-2020 Class Fu</p>
 </div>`;
 
+let center=`<ul>
+<li>
+      <div class="picture">
+            <img src="./img/25043752.gif" alt="">
+      </div>
+</li>
+<li>welcome to my blog...</li>
+<li>there are something and somebody...</li>
+<li>or something just like this... </li>
+<li>寫博客，記錄心得</li>
+<li>寫筆記，通曉代碼</li>
+<li>寫下once，展望future</li>
+</ul>
+<img src="./img/309617.jpg" alt="">
+<ul class="center_word">
+<li>闻道有先后</li>
+<li>术业有专攻</li>
+<li>外行看热闹</li>
+<li>内行看门道</li>
+</ul>
+<img src="./img/315441.jpg" alt="">
+<ul>
+<li>热闹：</li>
+<li>
+      <ul>
+            <li>
+                  <div class="picture">
+                        <img src="./img/25043751.gif" alt="">
+                  </div>
+            </li>
+      </ul>
+</li>
+<li>门道：</li>
+<li>
+      <ul>
+            <li>Web Server：liunx, centos 7.0.5 version, nginx</li>
+            <li>Building Tool：visual studio code, winscp, upupw, git</li>
+            <li>Coding Language：html, css, javascript, jquery, json</li>
+            <li>Code Store： github</li>
+      </ul>
+</li>
+</ul>
+<img src="./img/323822.jpg" alt="">
+<div class="text">
+<ul class="endpoem">
+      <li>滕王高阁临江渚，佩玉鸣鸾罢歌舞。</li>
+      <li>画栋朝飞南浦云，珠帘暮卷西山雨。</li>
+      <li>闲云潭影日悠悠，物换星移几度秋。</li>
+      <li>阁中帝子今何在？槛外长江空自流。</li>
+</ul>
+</div>`
+
