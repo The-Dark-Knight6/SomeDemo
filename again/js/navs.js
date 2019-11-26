af77d463e10a0cb468f6e8949bf43d48cf56ecb1
justone <645865839@qq.com>
2019-11-26 11:13:47 +0800

    ew23

--- again/js/navs.js
+++ again/js/navs.js
@@ -35,7 +35,7 @@ let navs =`
 <li>
     <div>
         <img src="./img/menu.png" class="top_img">
-        <span class="top_span">MOBA前世今生</span>
+        <span class="top_span">MOBA</span>
     </div>
     <ul>
         <li><a href="./jqtest3.html">验证码</a></li>
@@ -57,6 +57,7 @@ let navs =`
 let float=` <div class="float_tip">
 <div class="fly_cont">
     <p></p>
+    <h3 class="desc"></h3>
 </div>
 </div>`
 
@@ -94,10 +95,20 @@ window.onload=function(){
     let a_name=document.getElementsByTagName('a');
     let float_tip = document.querySelector('.float_tip');
     let fly_cont = document.querySelector('.fly_cont').children[0];
+    let fly_p = document.querySelector('.desc');
     for(let i = 0;i < a_name.length ; i++){
         a_name[i].addEventListener('mouseover',function(){
             float_tip.style.display='block';
             fly_cont.innerHTML = this.innerHTML;
+            switch(fly_cont.innerHTML){
+                case '首页':fly_p.innerHTML='所赖君子见机，达人知命。老当益壮，宁移白首之心？穷且益坚，不坠青云之志。';break;
+                case '自制列表':fly_p.innerHTML='寄蜉蝣于天地，渺沧海之一粟。哀吾生之须臾，羡长江之无穷。挟飞仙以遨游，抱明月而长终。';break;
+                case '图表':fly_p.innerHTML='惟江上之清风，与山间之明月，耳得之而为声，目遇之而成色，取之无禁，用之不竭，是造物者之无尽藏也，而吾与子之所共适。';break;
+                case '表格检索':fly_p.innerHTML='江流有声，断岸千尺；山高月小，水落石出。曾日月之几何，而江山不可复识矣。';break;
+                case '猜数字':fly_p.innerHTML='既自以心为形役，奚惆怅而独悲？悟已往之不谏，知来者之可追。';break;
+                case '按压密码':fly_p.innerHTML='江畔何人初见月？江月何年初照人？人生代代无穷已，江月年年望相似。';break;
+                default :fly_p.innerHTML='there\'s nothing...'
+            }
         })
         a_name[i].addEventListener('mouseleave',function(){
             float_tip.style.display='none';
